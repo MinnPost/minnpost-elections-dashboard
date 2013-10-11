62b4dd6103651ea6319dffb6629c67be42d54d0b
Alan Palazzolo <apalazzolo@minnpost.com>
2013-10-11 16:51:49 -0500

    Importing in areas and matching up most of the contests with specific boundary sets.

--- code/scraper.py
+++ code/scraper.py
@@ -86,7 +86,7 @@ class ElectionScraper:
           count = 0
           for row in rows:
             # Parse row
-            parsed = parser_method(row, i)
+            parsed = parser_method(row, i, s['table'])
 
             # Save to database
             try:
@@ -112,8 +112,73 @@ class ElectionScraper:
           self.log.info('[%s] Scraped rows for %s: %s' % (s['type'], i, count))
 
 
-  def parser_results(self, row, i):
-    # Make a UTC timestamp
+  def parser_areas(self, row, group, table):
+    """
+    Parser for areas type supporting tables.
+    """
+    timestamp = calendar.timegm(datetime.datetime.utcnow().utctimetuple())
+
+    # General data
+    parsed = {
+      'id': group + '-',
+      'areas_group': group,
+      'county_id': None,
+      'county_name': None,
+      'ward_id': None,
+      'precinct_id': None,
+      'precinct_name': '',
+      'state_senate_id': None,
+      'state_house_id': None,
+      'county_commissioner_id': None,
+      'district_court_id': None,
+      'soil_water_id': None,
+      'school_district_id': None,
+      'school_district_name': '',
+      'mcd_id': None,
+      'precincts': None,
+      'name': '',
+      'updated': int(timestamp)
+    }
+
+    if group == 'municipalities':
+      parsed['id'] =  parsed['id'] + row[0] + '-' + row[2]
+      parsed['county_id'] = row[0]
+      parsed['county_name'] = row[1]
+      parsed['mcd_id'] = row[2]
+      parsed['name'] = row[1]
+
+    if group == 'counties':
+      parsed['id'] =  parsed['id'] + row[0]
+      parsed['county_id'] = row[0]
+      parsed['county_name'] = row[1]
+      parsed['precincts'] = row[2]
+
+    if group == 'precincts':
+      parsed['id'] =  parsed['id'] + row[0] + '-' + row[1]
+      parsed['county_id'] = row[0]
+      parsed['precinct_id'] = row[1]
+      parsed['precinct_name'] = row[2]
+      parsed['state_senate_id'] = row[3]
+      parsed['state_house_id'] = row[4]
+      parsed['county_commissioner_id'] = row[5]
+      parsed['district_court_id'] = row[6]
+      parsed['soil_water_id'] = row[7]
+      parsed['mcd_id'] = row[8]
+
+    if group == 'school_districts':
+      parsed['id'] =  parsed['id'] + row[0]
+      parsed['school_district_id'] = row[0]
+      parsed['school_district_name'] = row[1]
+      parsed['county_id'] = row[2]
+      parsed['county_name'] = row[3]
+
+    return parsed
+
+
+  def parser_results(self, row, group, table):
+    """
+    Parser for results type scraping.
+    """
     timestamp = calendar.timegm(datetime.datetime.utcnow().utctimetuple())
 
     # Create ids
@@ -123,50 +188,72 @@ class ElectionScraper:
     row_id = base_id + '-' + row[6]
     id_name = base_id + '-' + row[6] + '-' + office_name_id + '-' + cand_name_id
 
-    # Office refers to office name and office id as assigned by SoS, but
-    # contest ID is a more specific id as office id's are not unique across
-    # all results
-    contest_id = base_id
-    contest_id_name = contest_id + '-' + office_name_id
+    # Check if records already exists, but first if table exists yet
+    found = False
+    found_table = scraperwiki.sqlite.select("name FROM sqlite_master WHERE type='table' AND name='%s'" % table)
+    if found_table != []:
+      found_rows = scraperwiki.sqlite.select("* FROM %s WHERE id = '%s'" % (table, row_id))
+      found = found_rows != []
 
-    # For ranked choice voting, we want to a consistent contest id, as the
-    # office_id is different for each set of choices.
-    #
-    # It seems that the office id is incremented by 1 starting at 1 so
-    # we use the first
-    ranked_choice = re.compile('.*(first|second|third|\w*th) choice.*', re.IGNORECASE).match(row[4])
-    if ranked_choice is not None:
-      office_id = ''.join(row[3].split())[:-1] + '1'
-      contest_id = 'id-' + row[0] + '-' + row[1] + '-' + row[2] + '-' + row[5] + '-' + office_id
+    found = False
+    if found:
+      return {
+        'id': row_id,
+        'precincts_reporting': int(row[11]),
+        'total_effected_precincts': int(row[12]),
+        'votes_candidate': int(row[13]),
+        'percentage': float(row[14]),
+        'total_votes_for_office': int(row[15]),
+        'updated': int(timestamp)
+      }
+
+    else:
+      # Office refers to office name and office id as assigned by SoS, but
+      # contest ID is a more specific id as office id's are not unique across
+      # all results
+      contest_id = base_id
       contest_id_name = contest_id + '-' + office_name_id
 
-    return {
-      'id': row_id,
-      'office_type': 'to-fix', # FIX
-      'state': row[0],
-      'county_id': row[1],
-      'precinct_id': row[2],
-      'office_id': row[3],
-      'office_name': row[4],
-      'district_code': row[5], # District, mcd, or fips code
-      'candidate_id': row[6],
-      'candidate': row[7].replace('WRITE-IN**', 'WRITE-IN'),
-      'suffix': row[8],
-      'incumbent_code': row[9],
-      'party_id': row[10],
-      'precincts_reporting': int(row[11]),
-      'total_effected_precincts': int(row[12]),
-      'votes_candidate': int(row[13]),
-      'percentage': float(row[14]),
-      'total_votes_for_office': int(row[15]),
-      'id_name': id_name,
-      'contest_id': contest_id,
-      'contest_id_name': contest_id_name,
-      'cand_name_id': cand_name_id,
-      'office_name_id': office_name_id,
-      'question_body': '',
-      'updated': int(timestamp)
-    }
+      # For ranked choice voting, we want to a consistent contest id, as the
+      # office_id is different for each set of choices.
+      #
+      # It seems that the office id is incremented by 1 starting at 1 so
+      # we use the first
+      ranked_choice = re.compile('.*(first|second|third|\w*th) choice.*', re.IGNORECASE).match(row[4])
+      if ranked_choice is not None:
+        office_id = ''.join(row[3].split())[:-1] + '1'
+        contest_id = 'id-' + row[0] + '-' + row[1] + '-' + row[2] + '-' + row[5] + '-' + office_id
+        contest_id_name = contest_id + '-' + office_name_id
+
+      parsed = {
+        'id': row_id,
+        'results_group': group,
+        'state': row[0],
+        'county_id': row[1],
+        'precinct_id': row[2],
+        'office_id': row[3],
+        'office_name': row[4],
+        'district_code': row[5], # District, mcd, or fips code
+        'candidate_id': row[6],
+        'candidate': row[7].replace('WRITE-IN**', 'WRITE-IN'),
+        'suffix': row[8],
+        'incumbent_code': row[9],
+        'party_id': row[10],
+        'precincts_reporting': int(row[11]),
+        'total_effected_precincts': int(row[12]),
+        'votes_candidate': int(row[13]),
+        'percentage': float(row[14]),
+        'total_votes_for_office': int(row[15]),
+        'id_name': id_name,
+        'contest_id': contest_id,
+        'contest_id_name': contest_id_name,
+        'cand_name_id': cand_name_id,
+        'office_name_id': office_name_id,
+        'question_body': '',
+        'updated': int(timestamp)
+      }
+
+      return parsed
 
 
   def index_results(self):
@@ -177,7 +264,7 @@ class ElectionScraper:
     self.log.info('[%s] Creating indices for results table.' % ('results'))
 
 
-  def post_results(self, i):
+  def post_results(self, group):
     # Update some vars for easy retrieval
     scraperwiki.sqlite.save_var('updated', int(calendar.timegm(datetime.datetime.utcnow().utctimetuple())))
     contests = scraperwiki.sqlite.select("COUNT(DISTINCT contest_id) AS contest_count FROM results")
@@ -191,12 +278,77 @@ class ElectionScraper:
       scraperwiki.sqlite.save_var('total_effected_precincts', state_contest[0]['total_effected_precincts'])
 
 
+  def boundary_match_results(self, parsed_row):
+    """
+    Logic to figure out what boundary the contest is for.  This will get messy.
+    """
+    boundary = ''
+
+    # School district is in the office name
+    if parsed_row['results_group'] == 'school_district_results':
+      isd_match = re.compile('.*\(ISD #([0-9]+)\).*', re.IGNORECASE).match(parsed_row['office_name'])
+      if isd_match is not None:
+        boundary = isd_match.group(1) + '-school-district-2013'
+      else:
+        self.log.info('[%s] Could not find ISD boundary for: %s' % ('results', parsed_row['office_name']))
+
+    # County should be provide, but the results also have results for county
+    # comissioner which are sub-county boundaries
+    if parsed_row['results_group'] == 'county_results':
+      comissioner_match = re.compile('.*Commissioner District.*', re.IGNORECASE).match(parsed_row['office_name'])
+      if comissioner_match is not None:
+        boundary = parsed_row['county_id'] + '-' + parsed_row['district_code'] + '-county-commissioner-district-2012'
+      else:
+        boundary = parsed_row['county_id'] + '-county-2010'
+
+    # This includes both municpal (city) level results, plus sub-municpal, such
+    # as city council results.
+    #
+    # For municpal results.  The boundary code is SSCCCMMMM where:
+    #   * SS is state ID which is 27
+    #   * CCC is the county FIPS code which is the MN County Code * 2 - 1
+    #   * MMMM is the municpal code
+    # The main issue is getting the county code which is not included in the
+    # results but instead in a separate table.
+    #
+    # It also turns out that there cities, like White Bear Lake City
+    # which is in multiple counties which means they have more than one
+    # boundary.
+    #
+    # For the sub-municpal results, we need wards.  Unfortunately the boundary
+    # id for wards is the actual name of the city and the ward number due to the
+    # face that the original boundary data did not have mcd codes in it.
+    if parsed_row['results_group'] == 'municipal_results':
+      # Check for sub municipal
+      wards_matched = re.compile('.*(Council Member Ward|Council Member District) ([0-9]+).*\((((?!elect).)*)\).*', re.IGNORECASE).match(parsed_row['office_name'])
+      if wards_matched is not None:
+        boundary = self.slugify(wards_matched.group(3)) + '-w-' + '{0:02d}'.format(int(wards_matched.group(2))) + '-ward-2012'
+      else:
+        if parsed_row['county_id']:
+          fips = '{0:03d}'.format((int(parsed_row['county_id']) * 2) - 1)
+          boundary = '27' + fips + parsed_row['district_code'] + '-minor-civil-division-2010'
+        else:
+          mcd = scraperwiki.sqlite.select("* FROM areas WHERE areas_group = 'municipalities' AND mcd_id = '%s'" % (parsed_row['district_code']))
+          if mcd != []:
+            boundaries = []
+            for r in mcd:
+              fips = '{0:03d}'.format((int(r['county_id']) * 2) - 1)
+              boundaries.append('27' + fips + parsed_row['district_code'] + '-minor-civil-division-2010')
+            boundary = ','.join(boundaries)
+          else:
+            self.log.info('[%s] Could not find corresponding county for municpality: %s' % ('results', parsed_row['office_name']))
+
+    return boundary
+
+
   def aggregate_results(self, *args):
     """
-    Given results, aggregate into a table of races.
+    Given results, aggregate into a table of races.  This is for the meta data
+    for each contest, not for the voting numbers, so it doesn't need to be run
+    every update of results.
     """
     index_created = False
-    results = scraperwiki.sqlite.select("DISTINCT contest_id, office_name FROM results")
+    results = scraperwiki.sqlite.select("DISTINCT contest_id, office_name, county_id, precinct_id, district_code, results_group FROM results")
 
     for r in results:
       # The only way to know if there are multiple seats is look at the office
@@ -225,6 +377,9 @@ class ElectionScraper:
         r['title'] = re_place.group(1) + ' ' + r['title']
       r['title'] = r['title'].rstrip()
 
+      # Match to a boundary or boundaries keys
+      r['boundary'] = self.boundary_match_results(r)
+
       # Remove office
       del r['office_name']
 
@@ -242,6 +397,12 @@ class ElectionScraper:
         self.log.exception('[%s] Error thrown while saving to table: %s' % ('contests', r))
         raise
 
+
+  def slugify(self, orig):
+    slug = orig.encode('ascii', 'ignore').lower()
+    slug = re.sub(r'[^a-z0-9]+', '-', slug).strip('-')
+    slug = re.sub(r'[-]+', '-', slug)
+    return slug
 
 # If calling directly
 if __name__ == "__main__":
