faad230f6502c52a0c6798b2a6090189bbab43a1
Alan Palazzolo <apalazzolo@minnpost.com>
2013-10-16 15:45:23 -0500

    Handling ranked choice voting display.

--- js/models.js
+++ js/models.js
@@ -8,7 +8,10 @@
       "ON c.contest_id = r.contest_id WHERE c.contest_id = '%CONTEST_ID%'",
 
     // Fields that are for contests (not result)
-    contestFields: ['boundary', 'contest_id', 'contest_id_name', 'county_id', 'district_code', 'office_id', 'office_name', 'office_name_id', 'precinct_id', 'precincts_reporting', 'question_body', 'ranked_choice', 'results_group', 'seats', 'state', 'title', 'total_effected_precincts', 'total_votes_for_office', 'updated'],
+    contestFields: ['boundary', 'contest_id', 'contest_id_name', 'county_id', 'district_code', 'office_id', 'office_name_id', 'precinct_id', 'precincts_reporting', 'question_body', 'ranked_choice', 'results_group', 'seats', 'state', 'title', 'total_effected_precincts', 'total_votes_for_office', 'updated'],
+
+    // Ranked choice places
+    rankedChoiceTranslations: { 'first': 1, 'second': 2, 'third': 3, 'fourth': 4, 'fifth': 5, 'sixth': 6, 'seventh': 7, 'eighth': 8, 'nineth': 9, 'tenth': 10, 'final': 100 },
 
     // Initializer
     initialize: function(model, options) {
@@ -42,7 +45,46 @@
         });
         parsed.results.push(result);
       });
-      parsed.results = _.sortBy(parsed.results, 'percentage');
+
+      // Ranked choice handling.  Group each candidate and add array
+      // for results per rank
+      if (parsed.ranked_choice) {
+        var groupedResults = {};
+        _.each(parsed.results, function(r) {
+          _.each(thisModel.rankedChoiceTranslations, function(c, choice) {
+            if (r.office_name.toLowerCase().indexOf(choice) > 0) {
+              // Group the results
+              groupedResults[r.candidate_id] = groupedResults[r.candidate_id] || {};
+              groupedResults[r.candidate_id].ranked_choices = groupedResults[r.candidate_id].ranked_choices || {};
+              groupedResults[r.candidate_id].ranked_choices[c] = {
+                'ranked_choice': c,
+                'percentage': r.percentage,
+                'votes_candidate': r.votes_candidate,
+                'office_name': r.office_name
+              };
+
+              // If the first choice, use this information to fill in results
+              if (c === 1) {
+                groupedResults[r.candidate_id] = _.extend(groupedResults[r.candidate_id], r);
+              }
+            }
+          });
+        });
+
+        parsed.results = _.values(groupedResults);
+
+        // TODO: Check who won.  This will have to be manually created and
+        // has yet to be determined yet
+      }
+      else {
+        // See who won by checking number of seats.
+      }
+
+      // Put results in a basic order.
+      parsed.results = _.sortBy(parsed.results, 'candidate');
+      parsed.results = _.sortBy(parsed.results, function(r) {
+        return r.percentage * -1;
+      });
 
       // Further formatting
       parsed.updated = moment.unix(parsed.updated);
