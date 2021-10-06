4c2a0b4fce0c9f056af1093fb3e048c56167c682
Jonathan Stegall <jstegall@minnpost.com>
2021-10-06 10:38:00 -0500

    start with that other tutorial https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world

---
+++ app/__init__.py
@@ -0,0 +1,40 @@
+import logging
+import os
+from flask import Flask, request, current_app
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    return app
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
+
+
+from app import models
