6a8c21de424aa58d9e3571768a0a9796a5991e90
Jonathan Stegall <jstegall@minnpost.com>
2022-08-05 11:12:15 -0500

    basic html structure for what we are going to want

---
+++ client/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<div class="election-results">
+	<header class="m-dashboard-header">
+		<h2 class="a-election-status">election date primary or general election results. Last updated on when it was updated</h2>
+	</header>
+
+	<div>
+		<ol>
+			<li>search form</li>
+			<li>suggested searches</li>
+		</ol>
+	</div>
+
+	<ol>
+		<li>show results
+			<ul>
+				<li>list of current result races</li>
+			</ul>
+			<ul>
+				<li>table of individual race's results</li>
+				<li>map</li>
+				<li>permalink to that race?</li>
+			</ul>
+		</li>
+	</ol>
+
+</div>
