59f0592f3dbe1d8650d6ea3d255ec0bd146f1513
Jonathan Stegall <jstegall@minnpost.com>
2022-08-05 09:50:11 -0500

    standalone dashboard array

---
+++ client/src/dashboard.js
@@ -0,0 +1,50 @@
+export let dashboard = [
+    {
+        title: 'Minneapolis Mayor',
+        type: 'race',
+        id: 'id-MN---43000-2001',
+        rows: 5
+    },
+    {
+        title: 'Minneapolis Question 1',
+        type: 'race',
+        id: 'id-MN---43000-1131',
+        rows: 2
+    },
+    {
+        title: 'Minneapolis Question 2',
+        type: 'race',
+        id: 'id-MN---43000-1132',
+        rows: 2
+    },
+    {
+        title: 'Minneapolis Question 3',
+        type: 'race',
+        id: 'id-MN---43000-1133',
+        rows: 2
+    },
+    {
+        title: 'St. Paul Question 1',
+        type: 'race',
+        id: 'id-MN---58000-1131',
+        rows: 2
+    },
+    {
+        title: 'Minneapolis Council Member — Ward 3',
+        type: 'race',
+        id: 'id-MN---43000-2121',
+        rows: 3
+    },
+    {
+        title: 'Minneapolis Council Member — Ward 9',
+        type: 'race',
+        id: 'id-MN---43000-2181',
+        rows: 3
+    },
+    {
+        title: 'Minneapolis Council Member — Ward 10',
+        type: 'race',
+        id: 'id-MN---43000-2191',
+        rows: 3
+    }
+];
