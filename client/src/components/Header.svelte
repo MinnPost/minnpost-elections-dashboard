c94aaa4ba562bca6d75d80640cae1044441d5a09
Jonathan Stegall <jstegall@minnpost.com>
2022-09-20 14:45:41 -0500

    fix boolean

--- client/src/components/Header.svelte
+++ client/src/components/Header.svelte
@@ -14,7 +14,7 @@
     <p>loading...</p>
 {:then electionData}
     <header class="m-dashboard-header">
-        <h2 class="a-election-status">Showing {electionData.date} {#if electionData.primary == "true"}primary{:else}general{/if} election results. Last updated on {electionData.updated}. Data last scraped on {electionData.scraped}</h2>
+        <h2 class="a-election-status">Showing {electionData.date} {#if electionData.primary == true}primary{:else}general{/if} election results. Last updated on {electionData.updated}. Data last scraped on {electionData.scraped}</h2>
         <p><small>(last value was  Last updated on 2022-08-04 11:13:28.05614-05)</small></p>
     </header>
 {:catch error}
