9d644727e83d1ba18a95d2a24b4e99092e5a8ca9
Jonathan Stegall <jstegall@minnpost.com>
2022-09-22 10:40:39 -0500

    use the updated heroku app

--- js/app.js
+++ js/app.js
@@ -23,12 +23,12 @@ require(['jquery', 'underscore', 'screenfull', 'base', 'helpers', 'views', 'rout
       interfaceRefresh: 1000 * 60 * 30,
       electionsAPIPollInterval: 50000,
       //electionsAPI: 'https://elections-scraper.minnpost.com/?box=ubuntu/minnpost-scraper-mn-election-results&method=sql&q=',
-      electionsAPI: '//minnpost-mn-election-results.herokuapp.com/api/query/?q=',
-      // Local: '//0.0.0.0:5000/api/?q='
+      electionsAPI: '//minnpost-mn-election-results.herokuapp.com/api/query/?election_id=id-20211102&q=',
+      // Local: '//0.0.0.0:5000/api/query/??election_id=id-20211102&q='
       // Custom: '//54.91.220.106/?box=ubuntu/minnpost-scraper-mn-election-results&method=sql&q='
       // MinnPost-specific: 'https://elections-scraper.minnpost.com/?box=ubuntu/minnpost-scraper-mn-election-results&method=sql&q='
       // ScraperWiki: '//premium.scraperwiki.com/ez47yoa/aaff8e67f921428/sql/?q='
-      // Heroku: https://minnpost-mn-election-results.herokuapp.com/api/query/?q=
+      // Heroku: 'https://minnpost-mn-election-results.herokuapp.com/api/query/?election_id=id-20211102&q='
       boundaryAPI: '//represent-minnesota.herokuapp.com/',
       boundarySets: [
         'minor-civil-divisions-2010',
