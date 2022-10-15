e8df58d45e30d6055868f2328ce3f873d05f7b48
Jonathan Stegall <jstegall@minnpost.com>
2022-10-15 14:55:07 -0500

    clean up the dashboard

--- client/src/data/dashboard.js
+++ client/src/data/dashboard.js
@@ -1,63 +1,22 @@
-// set the races
-let dashboardRaces = [
-    {
-        id: 'id-MN---3-0123'
-    },
-    {
-        id: 'id-MN---7-0127'
-    },
-    {
-        id: "id-MN---3-0106"
-    },
-    {
-        id: "id-MN----0335"
-    },
-    {
-        id: "id-MN----0332"
-    },
-    {
-        id: "id-MN---8-0111"
-    },
-    {
-        id: "id-MN---4-0107"
-    },
-    {
-        id: "id-MN---5-0108"
-    },
-    {
-        id: "id-MN---6-0109"
-    },
-    {
-        id: "id-MN----0333"
-    },
-    {
-        id: "id-MN---2-0105"
-    },
-    {
-        id: "id-MN---7-0110"
-    },
-    {
-        id: "id-MN----0331"
-    },
-    {
-        id: 'id-MN---43000-1132'
-    },
-    {
-        id: 'id-MN---43000-1133'
-    },
-    {
-        id: 'id-MN---58000-1131'
-    },
-    {
-        id: 'id-MN---43000-2121'
-    },
-    {
-        id: 'id-MN---43000-2181'
-    },
-    {
-        id: 'id-MN---43000-2191'
-    }
+// set the races we show on initial page load.
+export let dashboard = [
+    'id-MN---3-0123',
+    'id-MN---7-0127',
+    'id-MN---3-0106',
+    'id-MN----0335',
+    'id-MN----0332',
+    'id-MN---8-0111',
+    'id-MN---4-0107',
+    'id-MN---5-0108',
+    'id-MN---6-0109',
+    'id-MN----0333',
+    'id-MN---2-0105',
+    'id-MN---7-0110',
+    'id-MN----0331',
+    'id-MN---43000-1132',
+    'id-MN---43000-1133',
+    'id-MN---58000-1131',
+    'id-MN---43000-2121',
+    'id-MN---43000-2181',
+    'id-MN---43000-2191'
 ];
-
-// only use the ids. this might be unnecessary if we restructure.
-export let dashboard = dashboardRaces.map(function (el) { return el.id; });
