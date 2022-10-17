e1b076ad890eb5f23b7831a95eb8a001ffac4978
Jonathan Stegall <jstegall@minnpost.com>
2022-10-17 13:45:52 -0500

    fix some grid settings

--- client/src/components/Results.svelte
+++ client/src/components/Results.svelte
@@ -1,8 +1,11 @@
 <style>
+    :root {
+    --max-contest-columns: 2;
+    }
     .o-results {
         display: grid;
         gap: 1em;
-        grid-template-columns: repeat(auto-fit, minmax(10em, 25em));
+        grid-template-columns: repeat(auto-fit, minmax(20em, 1fr));
         list-style: none;
     }
     .a-election-status {
@@ -10,6 +13,11 @@
         color: #5e6e76;
         font-size: var(--scale-1);
         margin-bottom: 1.5em;
+    }
+    @media screen and (min-width: 60em) {
+        .o-results {
+            grid-template-columns: repeat(auto-fit, minmax(calc(100% / var(--max-contest-columns) - 1em), 1fr));
+        }
     }
 </style>
 
@@ -27,14 +35,16 @@
 	import Contest from "./Contest.svelte";
     import Advertisement from './Advertisement.svelte';
 
+    // language settings
     const pluralize = (count, noun, suffix = 's') => `${count} ${noun}${count !== 1 ? suffix : ''}`;
+    let label = 'contest';
 </script>
 
 {#await promise}
 	<p>Loading contests</p>
 {:then}
 {#key $resultStore}
-    <h3 class="a-election-status">Showing {pluralize($resultStore.length, 'contest')}</h3>
+    <h3 class="a-election-status">Showing {pluralize($resultStore.length, label)}</h3>
     <ul
         in:fade={{duration: 20}}
         out:fade={{duration: 20}
