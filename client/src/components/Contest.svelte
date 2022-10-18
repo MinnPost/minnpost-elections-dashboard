002420690ad80fbed71b33ed8ff6d9fb2cc76580
Jonathan Stegall <jstegall@minnpost.com>
2022-10-18 11:51:51 -0500

    need a link for debugging stuff

--- client/src/components/Contest.svelte
+++ client/src/components/Contest.svelte
@@ -7,6 +7,7 @@
 <li id="{contest.id}">
     <small>{contest.id}</small>
     <h3>{contest.title}</h3>
+    <p><a href="/contests/?scope={contest.scope}">{contest.scope}</a></p>
     {#if contest.sub_title !== null }
         <h4>{contest.sub_title}</h4>
     {/if}
