f4f1b8f88d7e099e3aa419a79cff1cbae8945cd3
Jonathan Stegall <jstegall@minnpost.com>
2022-11-01 10:38:20 -0500

    make a contest menu

---
+++ src/components/ContestMenu.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let contest;
+    export let label;
+    export let showVoteCount;
+    export let mapAvailable;
+    export let showMap;
+    
+    import {link} from 'svelte-spa-router';
+</script>
+
+<nav class="m-subnav-navigation m-topics-navigation m-menu-contest-groups">
+    <ul>
+        {#if (showVoteCount === false)}
+            <li><a href="/contest/?id={contest.id}" use:link>Full results for this {label}</a></li>
+        {:else}
+            <li><a href="/contest/?id={contest.id}" use:link>Show only this {label}</a></li>
+        {/if}
+        {#if (mapAvailable === true && showMap === false)}
+            <li><a href="/contest/?id={contest.id}" use:link>Show map for this {label}</a></li>
+        {/if}
+    </ul>
+</nav>
